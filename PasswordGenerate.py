519799e1cf76dc8becb15f6142b1f9c6940297d8
Kacper Grabowy <kacper.grabowy01@gmail.com>
2024-11-07 22:00:07 +0000

    1st 4 characaters of the password being saved to list

--- PasswordGenerate.py
+++ PasswordGenerate.py
@@ -9,11 +9,22 @@ import string
 # Making a special character variable
 special_characters = "!£$%^&*()_+[]@:#~<>,.?"
 
-# generating 1 random for each required character
-random_lowercase = random.choices(string.ascii_uppercase)
-random_uppercase = random.choices(string.ascii_lowercase)
-random_digit = random.choices(string.digits)
-random_special = random.choices(special_characters)
-
 # making a password list, will combine all characters and jumble at the end
 password = []
+
+# generating 1 random for each required character
+random_uppercase = random.choices(string.ascii_uppercase)
+password.append(random_uppercase)
+random_lowercase = random.choices(string.ascii_lowercase)
+password.append(random_lowercase)
+random_digit = random.choices(string.digits)
+password.append(random_digit)
+random_special = random.choices(special_characters)
+password.append(random_special)
+
+# testing if 4 random characters are chosen as attempted above
+print(password)
+
+for i in range(4):
+    i+1
+    random_number = random.randint(1, 4)
